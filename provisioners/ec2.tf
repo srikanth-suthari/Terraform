35a441c9d5e92912946329f4b6cb57c5250e8697
srikanth-suthari <suthari.srikanth@outlook.com>
2025-10-24 06:18:36 +0530

    Provisioners with local and remote-exec

--- provisioners/ec2.tf
+++ provisioners/ec2.tf
@@ -10,46 +10,46 @@ resource "aws_instance" "terraform" {
 	provisioner "local-exec" {
 		command = "echo Instance has been created"
 	}
-	
+
 	provisioner "local-exec" {
-		command = "echo ${self.private_ip} > inventory.ini"
+		command = "echo ${self.private_ip} >> inventory.ini"
 	}
 	provisioner "local-exec" {
      		command = "echo Instance is destroyed"
       		when    = destroy
 	}
-	connection {
-		type = "ssh"
-		user = "ec2-user"
-		password = "DevOps321"
-		host = self.public_ip
-	}
-	provisioner "remote-exec" {
-		inline = [
-			"sudo dnf update -y",
-			"sudo dnf install nginx -y",
-			"sudo systemctl start nginx"
-		]
-	}
-	
-	provisioner "remote-exec" {
-		inline = [
-			"sudo systemctl stop nginx",
-			"echo 'Successfully stopped nginx' "
-		]
-	} 
+	# connection {
+	# 	type = "ssh"
+	# 	user = "ec2-user"
+	# 	password = "DevOps321"
+	# 	host = self.public_ip
+	# }
+	# provisioner "remote-exec" {
+	# 	inline = [
+	# 		"sudo dnf update -y",
+	# 		"sudo dnf install nginx -y",
+	# 		"sudo systemctl start nginx"
+	# 	]
+	# }
+
+	# provisioner "remote-exec" {
+	# 	inline = [
+	# 		"sudo systemctl stop nginx",
+	# 		"echo 'Successfully stopped nginx' "
+	# 	]
+	# }
 }
 
 resource "aws_security_group" "allow_all" {
 	name = "allow-all"
-	
+
 	egress {
 		from_port = 0
 		to_port = 0
 		protocol = "-1"
 		cidr_blocks = ["0.0.0.0/0"]
 	}
-	
+
 	ingress {
 		from_port = 0
 		to_port = 0
